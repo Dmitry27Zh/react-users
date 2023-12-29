44f5a678d0b7d982c25fbe0692a612e1957733fa
Dmitry <dmitry27zh@gmail.com>
2023-12-29 10:54:01 +0500

    Load users

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Users from './components/Users'
 import Success from './components/Success'
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data.data))
+      .catch((err) => {
+        console.warn(err)
+        alert('Error')
+      })
+  }, [])
+
   return (
     <div className="App">
-      <Users />
+      <Users users={users} />
       {/* <Success /> */}
     </div>
   )
