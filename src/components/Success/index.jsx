4e0b7ac59a02d5f869152a14d1afcc9b461d1d0e
Dmitry <dmitry27zh@gmail.com>
2023-12-29 17:49:32 +0500

    Reload in Success

--- src/components/Success/index.jsx
+++ src/components/Success/index.jsx
@@ -4,7 +4,9 @@ const Success = ({ count }) => {
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем пользователям ({count}) отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <button className="send-invite-btn" onClick={() => window.location.reload()}>
+        Назад
+      </button>
     </div>
   )
 }
