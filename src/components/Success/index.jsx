aca4e02c7d2ce485e2fe0bc0be577661009498cf
Dmitry <dmitry27zh@gmail.com>
2023-12-29 10:48:54 +0500

    Extract Components

---
+++ src/components/Success/index.jsx
@@ -0,0 +1,12 @@
+const Success = () => {
+  return (
+    <div className="success-block">
+      <img src="/assets/success.svg" alt="Success" />
+      <h3>Успешно!</h3>
+      <p>Всем 4 пользователям отправлено приглашение.</p>
+      <button className="send-invite-btn">Назад</button>
+    </div>
+  )
+}
+
+export default Success
